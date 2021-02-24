a81b6827aebc1cb9af56d4194b8ae7aa26240e25
Mahaprasad Nayak <78096612+mahaprasadnayak@users.noreply.github.com>
2021-02-24 18:43:46 +0530

    Update EmailUtility.java

--- src/net/codejava/email/EmailUtility.java
+++ src/net/codejava/email/EmailUtility.java
@@ -13,11 +13,7 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-/**
- * A utility class for sending e-mail messages
- * @author www.codejava.net
- *
- */
+
 public class EmailUtility {
 	public static void sendEmail(String host, String port,
 			final String userName, final String password, String toAddress,
